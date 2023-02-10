228fe3a1b479cb9dfd01afeb9013ae853bf1c837
Max Hu <maxxh@google.com>
2023-02-10 21:34:58 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,52 @@
+# import requests
+from flask import Flask, request, send_from_directory, render_template
+import openai
+import os
+
+api_key = os.environ.get("api_key")
+if api_key:
+    openai.api_key = api_key
+else:
+    raise "No API key found in Environment Variable (api_key)"
+
+app = Flask(
+    __name__,
+)
+
+
+@app.route("/tryChat", methods=["GET", "POST"])
+def tryChat():
+    a = request.form.get("mydata")  # 获取ajax中mydata的内容，也就是输入的内容
+    word = str(a)
+    Chatword = chat(word)  # 返回应答
+    return Chatword
+
+
+def chat(text):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        # model="text-curie-001",
+        prompt=text,
+        temperature=0,
+        max_tokens=2000,
+    )
+    return response["choices"][0]["text"]
+
+
+# def smallchatbot(msg):
+#     url = f"http://api.qingyunke.com/api.php?key=free&appid=0&msg={msg}"  # 请求地址
+#     html = requests.get(url)
+#     content = html.json()["content"]
+#     con = str(content)
+#     c = con.replace("{br}", "\n")  # 对其返回的{br}进行转换转换为换行
+#     return c
+
+
+@app.route("/")
+def search():
+    # return send_from_directory("templates/", "ChatBot.html")
+    return render_template("ChatBot.html")
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ static/js/demo.js
@@ -0,0 +1,38 @@
+document.onkeydown = function () {
+	var e = window.event || arguments.callee.caller.arguments[0]   //  arguments.callee.caller.arguments[0]也相当于window.event的值
+	e.keyCode === 13 && sendmsg();
+}
+
+function sendmsg() {
+	var msg = ($("#ui_inp_msg").val());
+	if (msg == "") {
+		// this.tip({log:"addone",obj:$("#ui_inp_msg"),msg:"输入内容不能为空"});
+		alert("输入内容不能为空")
+		return;
+	}
+	$("#ui_inp_msg").val("");
+	var sendhtml = '<li class="t2"><img src="../static/images/icon.png"><div class="txt">' + msg + '</div></li>';
+	$("#ui_msg_box").append(sendhtml);
+	// 将展示消息的滚动条滑到底部
+	scrollMsgBottom();
+	$.ajax({
+		type: "POST",
+		url: "/tryChat",
+		data: { 'mydata': msg },
+		// dataType:"text",
+		success: function (data) {
+			var content = data
+			var sendhtml = '<li class="t1"><img src="../static/images/chatbot.png"><div class="txt">' + content + '</div></li>';
+			$("#ui_msg_box").append(sendhtml);
+			// 将展示消息的滚动条滑到底部
+			scrollMsgBottom();
+		},
+	});
+}
+function scrollMsgBottom() {
+	var topH = -$("#ui_msg_box").height();
+	$("#ui_msg_box>li").each(function () {
+		topH += $(this).outerHeight(true);
+	});
+	$("#ui_msg_box").animate({ scrollTop: topH }, 200);
+}
